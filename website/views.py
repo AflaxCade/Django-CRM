bf2b032a4546b2f931d8da9561c428af0b652d96
AflaxCade <aflah.cade@gmail.com>
2024-05-04 17:56:45 +0300

    added table in the home page

--- website/views.py
+++ website/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 
 def home(request):
+
+    # display all the records
+    records = Record.objects.all().values()
 
     # check to see if the user is logged in
 
@@ -24,7 +28,7 @@ def home(request):
             return redirect('home')
         
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 
 def register_user(request):
