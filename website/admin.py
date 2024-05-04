789aa96c5623b7c06b6d09601b0993b7e3cf4212
AflaxCade <aflah.cade@gmail.com>
2024-05-04 16:16:13 +0300

    created record model and added admin site

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Record
 
 # Register your models here.
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "phone", "email", "address", "city", "state", "zipcode", "created_at")
+
+admin.site.register(Record, RecordAdmin)
+
+
